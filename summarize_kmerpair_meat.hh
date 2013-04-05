961def3a4c76680d045b97da537b453155a9405d
Nathanael Fillmore <github@nate-fillmore.com>
2013-04-05 18:49:13 -0500

    Improved summarize kmerpair.
    - Especially in terms of runtime and code cleanliness.

--- summarize_kmerpair_meat.hh
+++ summarize_kmerpair_meat.hh
@@ -15,48 +15,67 @@
 #include "util.hh"
 using namespace std;
 
+struct for_A {};
+struct for_B {};
+struct wei {};
+struct unw {};
+
 struct Probs
 {
-  double for_A, for_B;
-  Probs() : for_A(0.0), for_B(0.0) {}
+  double data[2][2];
+
+  Probs()
+  {
+    data[0][0] = data[0][1] = data[1][0] = data[1][1] = 0.0;
+  }
+
+  template<typename for_A_or_B, typename wei_or_unw> inline       double& at();
+  template<typename for_A_or_B, typename wei_or_unw> inline const double& at() const;
 };
 
-//typedef boost::array<google::sparse_hash_map<size_t, Probs>, 16> KmerStats;
+template<> inline       double& Probs::at<for_A, wei>()       { return data[0][0]; }
+template<> inline       double& Probs::at<for_A, unw>()       { return data[0][1]; }
+template<> inline       double& Probs::at<for_B, wei>()       { return data[1][0]; }
+template<> inline       double& Probs::at<for_B, unw>()       { return data[1][1]; }
+
+template<> inline const double& Probs::at<for_A, wei>() const { return data[0][0]; }
+template<> inline const double& Probs::at<for_A, unw>() const { return data[0][1]; }
+template<> inline const double& Probs::at<for_B, wei>() const { return data[1][0]; }
+template<> inline const double& Probs::at<for_B, unw>() const { return data[1][1]; }
 
 struct KmerStats
 {
   boost::array<std::vector<Probs>, 16> data;
 
-  double& get_or_init(size_t pair, size_t dist, size_t A_or_B)
+  KmerStats(size_t max)
   {
-    if (dist >= data[pair].size())
-      data[pair].resize(std::max(dist+1, data[pair].size()*2));
-    if (A_or_B == 0)
-      return data[pair][dist].for_A;
-    else
-      return data[pair][dist].for_B;
+    for (size_t pair = 0; pair < 16; ++pair)
+      data[pair].resize(max);
   }
 };
 
 inline size_t choose_2(size_t n) { return n*(n-1)/2; }
 
-inline size_t encode(char c)
+size_t encode(char c)
 {
-  if (c == 'C')
-    return 0;
-  else if (c == 'G')
-    return 1;
-  else if (c == 'A')
-    return 2;
-  else if (c == 'T')
-    return 3;
-  else
-    return 4;
+  switch (c)
+  {
+    case 'C': return 0;
+    case 'G': return 1;
+    case 'A': return 2;
+    case 'T': return 3;
+    case 'c': return 0;
+    case 'g': return 1;
+    case 'a': return 2;
+    case 't': return 3;
+    default:  return 4;
+  }
 }
 
+// A_or_B is 0 if we're counting A's mers, 1 if B's
+template<typename for_A_or_B>
 void count_kmers(
     KmerStats& stats,
-    size_t A_or_B, // 0 if we're counting A's mers, 1 if B's
     const vector<string>& A,
     const vector<string>& A_rc,
     const vector<double>& tau_A)
@@ -70,15 +89,19 @@ void count_kmers(
     const vector<string>& X = (which == 0) ? A : A_rc;
     for (size_t i = 0; i < X.size(); ++i) {
       const string& a = X[i];
-      double c = 0.5*(1.0/choose_2(a.size()+1))*tau_A[i];
+      double c_weighted   = 0.5*(1.0/choose_2(a.size()+1))*tau_A[i];
+      double c_unweighted = 0.5*(1.0/choose_2(a.size()+1));
       string::const_iterator a_l, a_r, a_end = a.end();
       for (a_l = a.begin(); a_l != a_end; ++a_l) {
-        size_t dist = 1;
+        size_t dist = 0;
         for (a_r = a_l; a_r != a_end; ++a_r) {
           size_t i = encode(*a_l);
           size_t j = encode(*a_r);
-          if (i < 4 && j < 4)
-            stats.get_or_init(i+4*j, dist, A_or_B) += c; // relies on default init of Probs to {0.0, 0.0}
+          if (i < 4 && j < 4) {
+            Probs& probs = stats.data[i+4*j][dist];
+            probs.at<for_A_or_B, wei>() += c_weighted;   // relies on default init of Probs to all 0's
+            probs.at<for_A_or_B, unw>() += c_unweighted; // relies on default init of Probs to all 0's
+          }
           ++dist;
         }
       }
@@ -86,22 +109,18 @@ void count_kmers(
   }
 }
 
+template<typename for_A_or_B, typename wei_or_unw>
 void normalize_kmer_distributions(KmerStats& stats)
 {
-  double denom_for_A = 0.0, denom_for_B = 0.0;
-  for (size_t pair = 0; pair < 16; ++pair) {
-    BOOST_FOREACH(const Probs& probs, stats.data[pair]) {
-      denom_for_A += probs.for_A;
-      denom_for_B += probs.for_B;
-    }
-  }
+  double denom = 0.0;
 
-  for (size_t pair = 0; pair < 16; ++pair) {
-    BOOST_FOREACH(Probs& probs, stats.data[pair]) {
-      probs.for_A /= denom_for_A;
-      probs.for_B /= denom_for_B;
-    }
-  }
+  for (size_t pair = 0; pair < 16; ++pair)
+    BOOST_FOREACH(const Probs& probs, stats.data[pair])
+      denom += probs.at<for_A_or_B, wei_or_unw>();
+
+  for (size_t pair = 0; pair < 16; ++pair)
+    BOOST_FOREACH(Probs& probs, stats.data[pair])
+      probs.at<for_A_or_B, wei_or_unw>() /= denom;
 }
 
 void compute_kmer_stats(
@@ -113,11 +132,15 @@ void compute_kmer_stats(
     const vector<string>& B_rc,
     const vector<double>& tau_B)
 {
-  count_kmers(stats, 0, A, A_rc, tau_A);
-  count_kmers(stats, 1, B, B_rc, tau_B);
-  normalize_kmer_distributions(stats);
+  count_kmers<for_A>(stats, A, A_rc, tau_A);
+  count_kmers<for_B>(stats, B, B_rc, tau_B);
+  normalize_kmer_distributions<for_A, wei>(stats);
+  normalize_kmer_distributions<for_A, unw>(stats);
+  normalize_kmer_distributions<for_B, wei>(stats);
+  normalize_kmer_distributions<for_B, unw>(stats);
 }
 
+template<typename wei_or_unw>
 void print_kmer_stats(
     const KmerStats& stats,
     const string& prefix)
@@ -125,15 +148,26 @@ void print_kmer_stats(
   double KL_A_to_M = 0.0, KL_B_to_M = 0.0;
   for (size_t pair = 0; pair < 16; ++pair) {
     BOOST_FOREACH(const Probs& probs, stats.data[pair]) {
-      double mean_prob = 0.5*(probs.for_A + probs.for_B);
-      KL_A_to_M += probs.for_A == 0 ? 0 : probs.for_A * (log2(probs.for_A) - log2(mean_prob));
-      KL_B_to_M += probs.for_B == 0 ? 0 : probs.for_B * (log2(probs.for_B) - log2(mean_prob));
+      double prob_for_A = probs.at<for_A, wei_or_unw>();
+      double prob_for_B = probs.at<for_B, wei_or_unw>();
+      double mean_prob = 0.5*(prob_for_A + prob_for_B);
+      KL_A_to_M += prob_for_A == 0 ? 0 : prob_for_A * (log2(prob_for_A) - log2(mean_prob));
+      KL_B_to_M += prob_for_B == 0 ? 0 : prob_for_B * (log2(prob_for_B) - log2(mean_prob));
     }
   }
   double JS = 0.5*KL_A_to_M + 0.5*KL_B_to_M;
   cout << prefix << "_KL_A_to_M" << "\t" << KL_A_to_M << endl;
   cout << prefix << "_KL_B_to_M" << "\t" << KL_B_to_M << endl;
   cout << prefix << "_JS"        << "\t" << JS        << endl;
+}
+
+size_t max_length(const vector<string>& A)
+{
+  size_t mx = 0;
+  BOOST_FOREACH(const string& a, A)
+    if (a.size() > mx)
+      mx = a.size();
+  return mx;
 }
 
 void compute_and_print_kmer_stats(
@@ -145,9 +179,10 @@ void compute_and_print_kmer_stats(
     const vector<double>& tau_B,
     const string& prefix)
 {
-  KmerStats kmer_stats;
+  KmerStats kmer_stats(std::max(max_length(A), max_length(B)));
   compute_kmer_stats(kmer_stats, A, A_rc, tau_A, B, B_rc, tau_B);
-  print_kmer_stats(kmer_stats, prefix);
+  print_kmer_stats<wei>(kmer_stats, "weighted_" + prefix);
+  print_kmer_stats<unw>(kmer_stats, "unweighted_" + prefix);
 }
 
 char complement(char c)
@@ -210,8 +245,6 @@ void parse_options(boost::program_options::variables_map& vm, int argc, const ch
 
 void main_1(const boost::program_options::variables_map& vm)
 {
-  //clock_t start;
-
   std::cerr << "Reading the sequences" << std::endl;
   std::vector<std::string> A, B;
   std::vector<std::string> A_names, B_names;
@@ -236,17 +269,7 @@ void main_1(const boost::program_options::variables_map& vm)
   compute_nucl_expression(A, tau_A, nu_A);
   compute_nucl_expression(B, tau_B, nu_B);
 
-  std::cerr << "Computing uniform transcript-level expression" << std::endl;
-  std::vector<double> unif_tau_A(A.size(), 1.0/A.size());
-  std::vector<double> unif_tau_B(B.size(), 1.0/B.size());
-
-  std::cerr << "Computing uniform nucleotide-level expression" << std::endl;
-  std::vector<double> unif_nu_A(A.size()), unif_nu_B(B.size());
-  compute_nucl_expression(A, unif_tau_A, unif_nu_A);
-  compute_nucl_expression(B, unif_tau_B, unif_nu_B);
-
   std::cout << "summarize_kmerpair_version\t1" << std::endl;
 
-  compute_and_print_kmer_stats(A, A_rc, tau_A, B, B_rc, tau_B, "weighted_kmerpair");
-  compute_and_print_kmer_stats(A, A_rc, unif_tau_A, B, B_rc, unif_tau_B, "unweighted_kmerpair");
+  compute_and_print_kmer_stats(A, A_rc, tau_A, B, B_rc, tau_B, "kmerpair");
 }
